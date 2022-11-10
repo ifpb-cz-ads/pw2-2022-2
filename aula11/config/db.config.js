303e752caaff71eaa3d6fb6419a1a61fcac9f54f
Paulo Ewerton <pauloewerton@icloud.com>
2022-11-10 19:12:44 -0300

    Aulas 11 e 12

---
+++ aula11/config/db.config.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv').config();
+
+const url = process.env.DATABASE_URL;
+if (url.includes('localhost')) {
+  sequelize = new Sequelize(url);
+} else {
+  sequelize = new Sequelize(`${url}?sslmode=no-verify`, {
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  });
+}
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.contato = require('../models/contato')(sequelize, Sequelize);
+db.user = require('../models/user')(sequelize, Sequelize);
+
+db.sync = async() => {
+  await sequelize.sync();
+};
+
+module.exports = db;
